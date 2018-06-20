e409703f7bcb3ceb9a7c7c70b5f26f276d3305a4
Michael Vitz <michael.vitz@innoq.com>
2018-06-20 10:56:44 +0200

    feat: Add FlashMapDeserializer

---
+++ src/main/java/com/innoq/spring/cookie/flash/FlashMapDeserializer.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright 2018 innoQ Deutschland GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.innoq.spring.cookie.flash;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import org.springframework.web.servlet.FlashMap;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+final class FlashMapDeserializer extends StdDeserializer<FlashMap> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public FlashMapDeserializer() {
+        super(FlashMap.class);
+    }
+
+    @Override
+    public FlashMap deserialize(JsonParser p, DeserializationContext ctxt)
+            throws IOException {
+        final JsonNode root = objectMapper.readTree(p);
+        final FlashMap flashMap = new FlashMap();
+
+        final JsonNode attributes = root.findValue("attributes");
+        handleAttributes(flashMap, attributes);
+
+        final JsonNode expirationTime = root.findValue("expirationTime");
+        handleExpirationTime(flashMap, expirationTime);
+
+        final JsonNode targetRequestParams = root.findValue("targetRequestParams");
+        handleTargetRequestParams(flashMap, targetRequestParams);
+
+        final JsonNode targetRequestPath = root.findValue("targetRequestPath");
+        handleTargetRequestPath(flashMap, targetRequestPath);
+
+        return flashMap;
+    }
+
+    private void handleAttributes(FlashMap flashMap, JsonNode node) {
+        if (node == null || !node.isObject()) {
+            return;
+        }
+
+        final Map<String, Object> attributes =
+            objectMapper.convertValue(node, new TypeReference<Map<String, Object>>() {});
+        flashMap.putAll(attributes);
+    }
+
+    private void handleExpirationTime(FlashMap flashMap, JsonNode node) {
+        if (node == null || !node.isNumber()) {
+            return;
+        }
+
+        final long expirationTime = node.asLong();
+        flashMap.setExpirationTime(expirationTime);
+    }
+
+    private void handleTargetRequestParams(FlashMap flashMap, JsonNode node) {
+        if (node == null || !node.isObject()) {
+            return;
+        }
+
+        final Map<String, List<String>> targetRequestParams =
+            objectMapper.convertValue(node, new TypeReference<Map<String, List<String>>>() {});
+        targetRequestParams.forEach((key, values) ->
+            values.forEach(value ->
+                flashMap.addTargetRequestParam(key, value)));
+    }
+
+    private void handleTargetRequestPath(FlashMap flashMap, JsonNode node) {
+        if (node == null || !node.isTextual()) {
+            return;
+        }
+
+        final String targetRequestPath = node.asText();
+        flashMap.setTargetRequestPath(targetRequestPath);
+    }
+}
---
+++ src/test/java/com/innoq/spring/cookie/flash/FlashMapDeserializerTest.java
@@ -0,0 +1,175 @@
+/**
+ * Copyright 2018 innoQ Deutschland GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.innoq.spring.cookie.flash;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.servlet.FlashMap;
+
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+
+class FlashMapDeserializerTest {
+
+    ObjectMapper sut = new ObjectMapper()
+        .registerModule(
+            new SimpleModule().addDeserializer(FlashMap.class, new FlashMapDeserializer()));
+
+    @Test
+    void empty_flash_map_is_deserialized() throws Exception {
+        String json =
+            "{" +
+                "\"attributes\":{}," +
+                "\"expirationTime\":-1," +
+                "\"targetRequestParams\":{}," +
+                "\"targetRequestPath\":null" +
+            "}";
+
+        FlashMap flashMap = sut.readValue(json, FlashMap.class);
+
+        assertThat((Map<String, Object>) flashMap).isEmpty();
+        assertThat(flashMap.getExpirationTime()).isEqualTo(-1);
+        assertThat(flashMap.getTargetRequestParams()).isEmpty();
+        assertThat(flashMap.getTargetRequestPath()).isNull();
+    }
+
+    @Test
+    void complex_flash_map_is_deserialized() throws Exception {
+        String json =
+            "{" +
+                "\"attributes\":{" +
+                    "\"foo\":null," +
+                    "\"bar\":4711," +
+                    "\"baz\":\"lorem ipsum\"" +
+                "}," +
+                "\"expirationTime\":4711," +
+                "\"targetRequestParams\":{" +
+                    "\"foo\":[]," +
+                    "\"bar\":[\"foo\"]," +
+                    "\"baz\":[\"lorem\",\"ipsum\"]" +
+                "}," +
+                "\"targetRequestPath\":\"/foo\"" +
+            "}";
+
+        FlashMap flashMap = sut.readValue(json, FlashMap.class);
+
+        assertThat((Map<String, Object>) flashMap).containsOnly(
+            entry("foo", null), entry("bar", 4711), entry("baz", "lorem ipsum"));
+        assertThat(flashMap.getExpirationTime()).isEqualTo(4711);
+        assertThat(flashMap.getTargetRequestParams()).containsOnly(
+            entry("bar", asList("foo")), entry("baz", asList("lorem", "ipsum")));
+        assertThat(flashMap.getTargetRequestPath()).isEqualTo("/foo");
+    }
+
+    @Test
+    void unknown_fields_are_ignored() throws Exception {
+        String json =
+            "{" +
+                "\"foo\":{}," +
+                "\"bar\":-1," +
+                "\"baz\":null," +
+                "\"lorem\":[]" +
+            "}";
+
+        FlashMap flashMap = sut.readValue(json, FlashMap.class);
+
+        assertThat((Map<String, Object>) flashMap).isEmpty();
+        assertThat(flashMap.getExpirationTime()).isEqualTo(-1);
+        assertThat(flashMap.getTargetRequestParams()).isEmpty();
+        assertThat(flashMap.getTargetRequestPath()).isNull();
+    }
+
+    @Test
+    void in_list_can_still_be_deserialized() throws Exception {
+        String json =
+            "[" +
+                "{" +
+                    "\"foo\":{}," +
+                    "\"bar\":-1," +
+                    "\"baz\":null," +
+                    "\"lorem\":[]" +
+                "}," +
+                "{" +
+                    "\"attributes\":{" +
+                        "\"foo\":null," +
+                        "\"bar\":4711," +
+                        "\"baz\":\"lorem ipsum\"" +
+                    "}," +
+                    "\"expirationTime\":4711," +
+                    "\"foo\":{\"bar\":{\"baz\":4711}}," +
+                    "\"lorem\":[]," +
+                    "\"targetRequestParams\":{" +
+                        "\"foo\":[]," +
+                        "\"bar\":[\"foo\"]," +
+                        "\"baz\":[\"lorem\",\"ipsum\"]" +
+                    "}," +
+                    "\"targetRequestPath\":\"/foo\"" +
+                "}," +
+                "{" +
+                    "\"attributes\":{}," +
+                    "\"expirationTime\":-1," +
+                    "\"targetRequestParams\":{}," +
+                    "\"targetRequestPath\":null" +
+                "}," +
+                "{" +
+                    "\"attributes\":{}," +
+                    "\"foo\":{}," +
+                    "\"expirationTime\":-1," +
+                    "\"bar\":-1," +
+                    "\"targetRequestParams\":{}," +
+                    "\"baz\":null," +
+                    "\"targetRequestPath\":null," +
+                    "\"lorem\":[]" +
+                "}" +
+            "]";
+
+        List<FlashMap> flashMaps = sut.readValue(json, new TypeReference<List<FlashMap>>() {});
+
+        assertThat(flashMaps).hasSize(4);
+
+        FlashMap flashMap = flashMaps.get(0);
+        assertThat((Map<String, Object>) flashMap).isEmpty();
+        assertThat(flashMap.getExpirationTime()).isEqualTo(-1);
+        assertThat(flashMap.getTargetRequestParams()).isEmpty();
+        assertThat(flashMap.getTargetRequestPath()).isNull();
+
+        flashMap = flashMaps.get(1);
+        assertThat((Map<String, Object>) flashMap).containsOnly(
+            entry("foo", null), entry("bar", 4711), entry("baz", "lorem ipsum"));
+        assertThat(flashMap.getExpirationTime()).isEqualTo(4711);
+        assertThat(flashMap.getTargetRequestParams()).containsOnly(
+            entry("bar", asList("foo")), entry("baz", asList("lorem", "ipsum")));
+        assertThat(flashMap.getTargetRequestPath()).isEqualTo("/foo");
+
+        flashMap = flashMaps.get(2);
+        assertThat((Map<String, Object>) flashMap).isEmpty();
+        assertThat(flashMap.getExpirationTime()).isEqualTo(-1);
+        assertThat(flashMap.getTargetRequestParams()).isEmpty();
+        assertThat(flashMap.getTargetRequestPath()).isNull();
+
+        flashMap = flashMaps.get(3);
+        assertThat((Map<String, Object>) flashMap).isEmpty();
+        assertThat(flashMap.getExpirationTime()).isEqualTo(-1);
+        assertThat(flashMap.getTargetRequestParams()).isEmpty();
+        assertThat(flashMap.getTargetRequestPath()).isNull();
+    }
+}
